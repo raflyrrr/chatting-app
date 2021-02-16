76b3230be022ae336172d17d5caa1203f2398f85
Rafli Ramadhan <raflyr44@gmail.com>
2021-02-16 21:33:56 +0700

    cr

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,46 +1,68 @@
-import {useState} from 'react'
-import axios from 'axios'
+import { useState } from "react";
+import axios from "axios";
+import MadeWithLove from "react-made-with-love";
 
 const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const [username,setUsername] = useState('')
-    const [password,setPassword] = useState('')
-    const [error,setError] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e)=>{
-        e.preventDefault()
+    const authObject = {
+      "Project-ID": "da32c3d5-0940-483c-9596-5b5711bd6493",
+      "User-Name": username,
+      "User-Secret": password,
+    };
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
 
-        const authObject = {'Project-ID': "da32c3d5-0940-483c-9596-5b5711bd6493", 'User-Name': username, 'User-Secret': password}
-        try{
-            await axios.get('https://api.chatengine.io/chats',{headers: authObject})
-            localStorage.setItem('username',username)
-            localStorage.setItem('password',password)
-
-            window.location.reload()
-            setError('')
-        }catch (error){
-            setError('Oops, incorrect credentials.')
-        }
+      window.location.reload();
+      setError("");
+    } catch (error) {
+      setError("Oops, incorrect credentials.");
     }
-    
+  };
 
-    return (
+  return (
     <div className="wrapper">
-        <div className="form">
-            <h1 className="title">Chat Application</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={username}onChange={(e)=>setUsername(e.target.value)} className="input" placeholder="Username" required/>
-                <input type="password" value={password}onChange={(e)=>setPassword(e.target.value)} className="input" placeholder="Password" required/>
-                <div align="center">
-                    <button type="submit" className="button">
-                        <span>Start Chatting</span>
-                    </button>
-                </div>
-                <h2 className="error">{error}</h2>
-            </form>
+      <div className="form">
+        <h1 className="title">React Chat App</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start Chatting</span>
+            </button>
+          </div>
+          <h2 className="error">{error}</h2>
+        </form>
+        <div align="center" style={{color:'white'}}>
+          <MadeWithLove by="Rafli Ramadhan" emoji />
         </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
